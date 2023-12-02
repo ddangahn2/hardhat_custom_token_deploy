28997b928ab0423ce428336b116d41ffe80d65ae
Sangahn <sangahn0524@gmail.com>
2023-12-02 13:30:30 +0900

    Modify: deploy_gpt.js to deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,27 @@
-async function main() {
-    const [deployer] = await ethers.getSigners();
+const { ethers } = require("hardhat");
 
-    // HSAToken 컨트랙트의 생성자를 변수 HSAToken에 저장
-    const HSAToken = await ethers.getContractFactory("HSAToken");
-    // HSAToken 컨트랙트 배포
-    const HSA = await HSAToken.deploy();
-  }
+async function main() {
+  // 스마트 계약을 배포할 때 사용할 계정을 얻어옵니다.
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  // console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  // 스마트 계약의 팩토리를 얻어옵니다.
+  const HSAToken = await ethers.getContractFactory("HSAToken");
+
+  // 스마트 계약을 배포합니다.
+  const hsa = await HSAToken.deploy("Sangahn Han","HSA");
+
   
-  main() // main 함수를 호출하고 함수 실행이 완료되면 프로세스 종료, 에러가 발생하면 콘솔에 출력하고 프로세스를 에러코드 1로 종료
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
+  console.log("HSAToken deployed to:", hsa.target);
+}
+
+// 스크립트 실행
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error("Error during deployment:", error);
+    process.exit(1);
+  });
